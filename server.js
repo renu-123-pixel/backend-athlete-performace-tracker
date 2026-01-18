39c51af44d915add4931de46e45a79ec66bf8ff1
Renu Ekka <renuekka87836@gmail.com>
2026-01-19 02:25:52 +0530

    added port correctly

--- server.js
+++ server.js
@@ -29,8 +29,7 @@ app.use("/leaderboard", leaderboardRoutes);
 app.use("/api/athletes", athleteRoutes);
 
 app.get("/", (req, res) => res.send("Peak Athlete Backend Running"));
-
-const PORT = 5000;
+ const PORT = process.env.PORT || 5000;;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
 
